9cde62f4862c4eea377e1313c871b3feb64bf0a3
Rinthout <95086904+Rinthout@users.noreply.github.com>
2023-03-08 11:54:22 +0100

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import "../styles/home.css"
 function Header() {
     return (
         <div className="header-container">
-            <a href="https://www.wincacademy.com" alt="WincLogo">
-                <img src={logo} />
+            <a href="https://www.wincacademy.com" >
+                <img src={logo} alt="WincLogo" />
             </a>
             <h2>Winc Student Dashboard</h2>
         </div>
